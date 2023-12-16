0aeef2a4b8b32c049b8a6a112bd1489d8cbcfdf0
AdiSastrawan <adisastrawan110703@gmail.com>
2023-12-16 11:18:20 +0800

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const Hapi = require("@hapi/hapi")
 const NotesService = require("./services/inMemory/NotesService")
 const notes = require("./api/notes")
+const NotesValidator = require("./validator/notes")
 
 const init = async () => {
   const notesService = new NotesService()
@@ -14,7 +15,7 @@ const init = async () => {
       },
     },
   })
-  await server.register({ plugin: notes, options: { service: notesService } })
+  await server.register({ plugin: notes, options: { service: notesService, validator: NotesValidator } })
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
 }
